b349418da4ca79a30795ea8257895f39a218f9eb
Sameh <sameh.s.n.hanna@gmail.com>
2022-07-17 16:08:45 +0200

    Reformat SC5

--- src/test/java/org/example/pages/P_005.java
+++ src/test/java/org/example/pages/P_005.java
@@ -15,9 +15,11 @@ public class P_005 {
     public List<WebElement> getSubCategories() {
         return Hooks.driver.findElements(By.className("first-level"));
     }
-    public WebElement goToSubCategory(String subcat ) {
+
+    public WebElement goToSubCategory(String subcat) {
         return Hooks.driver.findElement(By.linkText(subcat));
     }
+
     public WebElement getTextOfPageTittle() {
         return Hooks.driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/div[3]/div/div[1]/h1"));
     }
--- src/test/java/org/example/stepDefs/SC_005.java
+++ src/test/java/org/example/stepDefs/SC_005.java
@@ -7,6 +7,7 @@ import org.example.pages.P_005;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -14,13 +15,13 @@ import java.util.Random;
 public class SC_005 {
 
     P_005 hover = new P_005();
-    String randomSubCategory ;
+    String randomSubCategory;
 
     @When("User select a random category from {string}, {string}, {string}")
     public void userSelectARandomCategoryFrom(String category1, String category2, String category3) {
 
-        String[] categories = new String [] {category1,category2,category3};
-        int randomIndexCategory =  new Random().nextInt(categories.length);
+        String[] categories = new String[]{category1, category2, category3};
+        int randomIndexCategory = new Random().nextInt(categories.length);
         Actions builder = new Actions(Hooks.driver);
         builder.moveToElement(hover.hoverOverACategory(categories[randomIndexCategory])).perform();
     }
@@ -29,7 +30,7 @@ public class SC_005 {
     public void userSelectARandomSubCategoryFromOneOfTheCategories() {
         List<String> subCat = new ArrayList<>();
         List<WebElement> searchResults = hover.getSubCategories();
-        for ( WebElement result : searchResults ) {
+        for (WebElement result : searchResults) {
             if (!result.getText().equals("")) {
                 subCat.add(result.getText());
             }
@@ -37,8 +38,8 @@ public class SC_005 {
         String[] objects = subCat.toArray(subCat.toArray(new String[0]));
         String lines11 = objects[0];
         String[] lines99 = lines11.split("\r?\n|\r");
-        int randomIndexSubCategory =  new Random().nextInt(lines99.length);
-        randomSubCategory = lines99[randomIndexSubCategory] ;
+        int randomIndexSubCategory = new Random().nextInt(lines99.length);
+        randomSubCategory = lines99[randomIndexSubCategory];
         hover.goToSubCategory((lines99[randomIndexSubCategory])).click();
     }
 
